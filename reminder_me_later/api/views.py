a1c2f2b2cc9af4bab388e31bdfa16d63110dc2e7
debasisbisoyi <168573583+debasisbisoyi@users.noreply.github.com>
2025-05-10 15:18:44 +0530

    Update views.py

--- reminder_me_later/api/views.py
+++ reminder_me_later/api/views.py
@@ -1,7 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
 from .models import Reminder
 from .serializers import ReminderSerializer
 
